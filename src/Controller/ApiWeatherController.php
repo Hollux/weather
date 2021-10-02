be12e2d6f68218e4845ec74dc0fc51202d2c25ab
hollux@hotmail.fr <hollux@hotmail.fr>
2021-10-02 08:30:40 +0200

    place dans le controller

--- src/Controller/ApiWeatherController.php
+++ src/Controller/ApiWeatherController.php
@@ -11,80 +11,34 @@ use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 use App\Service\WeatherTools;
-use App\Entity\WeatherVille; 
 
 class ApiWeatherController extends AbstractController
 {
-    private $client;
     private $weatherTools;
 
-    public function __construct(HttpClientInterface $client, WeatherTools $weatherTools)
+    public function __construct(WeatherTools $weatherTools)
     {
-        $this->client = $client;
         $this->weatherTools = $weatherTools;
     }
 
      /**
-     * @Route("/api_weather_villes", name="api_weather_villes")
+     * @Route("/api_weather_villes/{mode}", 
+     * defaults={"mode": "strict"} ,
+     * name="api_weather_villes", 
+     * requirements={"mode"="strict|degressif"},
+     * methods="POST")
      */
-    public function api_weather_villes(Request $request)
+    public function api_weather_villes($mode, Request $request)
     {
-        $date = date("jnY");
+        //pour fonctionner : "data" => ["ville1", "ville2"]
         $data = json_decode($request->getContent(), true)["data"];
 
-        $resp = [];
-        foreach ($data as $key => $city) {
-
-            $url = "https://api-adresse.data.gouv.fr/search/?q=" . $city;
-            $cityArray = $this->weatherTools->getClientResponse($this->client, $url);
-            if(!$cityArray){
-                return $this->json([
-                    "error" => "Ville ". $city . " introuvable",
+        $resp = $this->weatherTools->GetRespFromData($data);
+        if(isset($resp['error'])) {
+            return $this->json([
+                    "error" => $resp['error']
                 ]);
-            }
-
-            if(isset($cityArray["features"]) && isset($cityArray["features"][0]['geometry']["coordinates"])){
-                // verif si on a bien des infos des villes.
-
-                // On cherche les infos en bdd si possible pour éviter de consommer l'api.
-                // Les infos sont journalières pour avoir des données météo fraiches, mais pas trop non plus
-                // (un équilibre entre consommation d'api et informations à jour)
-                $weatherVille = $this->getDoctrine()
-                    ->getRepository(WeatherVille::class)
-                    ->findOneBy(["date" => $date, "city" => $cityArray["features"][0]["properties"]["label"]]);
-
-                if($weatherVille) {
-                    $resp[] = $weatherVille->getWeather();
-                } else {
-
-                    $weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=".
-                    $cityArray["features"][0]['geometry']["coordinates"][1]."&lon=".
-                    $cityArray["features"][0]['geometry']["coordinates"][0]."&exclude=minutely,hourly&appid=".
-                    $this->getParameter('weatherApiKey')."&lang=fr&units=metric";
-
-                    $weatherArray = $this->weatherTools->getClientResponse($this->client, $weatherUrl);
-
-                    //TODO tester le retour de l'api
-
-                    $resp[] = $this->weatherTools->getTHN($weatherArray["daily"]);
-
-                    //save des valeurs.
-                    $weatherVille = new WeatherVille;
-                    $weatherVille->setCity($cityArray["features"][0]["properties"]["label"]);
-                    $weatherVille->setDate($date);
-                    $weatherVille->setWeather($resp[$key]);
-                    $entityManager = $this->getDoctrine()->getManager();
-                    $entityManager->persist($weatherVille);
-                    $entityManager->flush();
-                }
-
-            } else {
-                return $this->json([
-                    "error" => "Ville ". $value . " introuvable",
-                ]);
-            } 
         }
 
         dump($resp);exit;
--- src/Service/WeatherTools.php
+++ src/Service/WeatherTools.php
@@ -1,8 +1,77 @@
 <?php
 namespace App\Service;
 
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\WeatherVille; 
+
 class WeatherTools
 {
+    private $entityManager;
+    private $client;
+
+    public function __construct(EntityManagerInterface $entityManager, HttpClientInterface $client)
+    {
+        $this->client = $client;
+        $this->em = $entityManager;
+    }
+
+    public function GetRespFromData($data){
+        if(!isset($data[0]) && !isset($data[1]) && count($data) !== 2){
+            return ["error" => "Configuration de data incorrecte"];
+        }
+        $date = date("jnY");
+        $resp = [];
+
+        foreach ($data as $key => $city) {
+
+            $url = "https://api-adresse.data.gouv.fr/search/?q=" . $city;
+            $cityArray = $this->getClientResponse($this->client, $url);
+            if(!$cityArray){
+                return ["error" => "Ville ". $city . " introuvable"];
+            }
+
+            if(isset($cityArray["features"]) && isset($cityArray["features"][0]['geometry']["coordinates"])){
+                // verif si on a bien des infos des villes.
+
+                // On cherche les infos en bdd si possible pour éviter de consommer l'api.
+                // Les infos sont journalières pour avoir des données météo fraiches, mais pas trop non plus
+                // (un équilibre entre consommation d'api et informations à jour)
+                 $weatherVille = $this->em->getRepository(WeatherVille::class)
+                            ->findOneBy(["date" => $date, "city" => $cityArray["features"][0]["properties"]["label"]]);
+
+                if($weatherVille) {
+                    $resp[] = $weatherVille->getWeather();
+                } else {
+
+                    $weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=".
+                    $cityArray["features"][0]['geometry']["coordinates"][1]."&lon=".
+                    $cityArray["features"][0]['geometry']["coordinates"][0]."&exclude=minutely,hourly&appid=".
+                    $this->getParameter('weatherApiKey')."&lang=fr&units=metric";
+
+                    $weatherArray = $this->getClientResponse($this->client, $weatherUrl);
+
+                    //TODO tester le retour de l'api
+
+                    $resp[] = $this->getTHN($weatherArray["daily"]);
+
+                    //save des valeurs.
+                    $weatherVille = new WeatherVille;
+                    $weatherVille->setCity($cityArray["features"][0]["properties"]["label"]);
+                    $weatherVille->setDate($date);
+                    $weatherVille->setWeather($resp[$key]);
+                    $this->em->persist($weatherVille);
+                    $this->em->flush();
+                }
+
+            } else {
+                return ["error" => "Ville ". $value . " introuvable"];
+            } 
+        }
+
+        return $resp;
+    }
+
     public function getClientResponse($client, $url){
 
         $response = $client->request(
